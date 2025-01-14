50c6eceacf8afe785de5422cbcab126bf279f7fd
阿貴/池雲寅 <90450779+Adam294577@users.noreply.github.com>
2025-01-14 11:12:48 +0800

    fix:  fetch list error

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,33 @@ function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [buttonText, setButtonText] = useState('Open Todo List Dialog');
 
-  const handleClick = () => {
+  const handleClick = async () => {
     setButtonText('Loading...');
     try {
+      await fetchTodoData();
       setIsOpen(true);
+      setButtonText('Open Todo List Dialog');
     } catch (_) {
-      setButtonText('error: Please check localhost server');
+      setButtonText('error: check localhost server !');
       setTimeout(() => {
         setButtonText('Open Todo List Dialog');
       }, 3000);
-      return;
     }
-    setButtonText('Open Todo List Dialog');
+  };
+
+  const fetchTodoData = async () => {
+    const response = await fetch('http://localhost:3000/todos');
+    if (!response.ok) {
+      throw new Error('error: check localhost server !');
+    }
+    return await response.json();
   };
 
   return (
     <div className='layout bg-[#555] h-screen xl:p5'>
       <div className='flex-center h-screen fixed inset-0'>
         <button
-          className='OpenBtn flex-center border w50 mxa py2 rounded-md bg-white'
+          className='OpenBtn flex-center border w70 mxa py2 rounded-md bg-white'
           onClick={handleClick}
         >
           {buttonText}
