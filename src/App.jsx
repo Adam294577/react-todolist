9df6f3da68748ef3d2cbfc7720f77844f48a1e6a
阿貴/池雲寅 <90450779+Adam294577@users.noreply.github.com>
2025-01-14 10:43:47 +0800

    chore: 載入需要模擬由server獲得清單工具

--- src/App.jsx
+++ src/App.jsx
@@ -3,50 +3,7 @@ import TodoDialog from './components/TodoDialog';
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: 'Learn React',
-      canceled: false,
-      completed: true,
-      createtime: '2025-01-01T00:00:00',
-    },
-    {
-      id: 2,
-      text: 'Learn Vue',
-      canceled: false,
-      completed: true,
-      createtime: '2025-01-01T00:00:01',
-    },
-    {
-      id: 3,
-      text: 'Learn Angular',
-      canceled: false,
-      completed: false,
-      createtime: '2025-01-01T00:00:02',
-    },
-    {
-      id: 4,
-      text: 'Learn Svelte',
-      canceled: false,
-      completed: false,
-      createtime: '2025-01-01T00:00:03',
-    },
-    {
-      id: 5,
-      text: 'Learn Next',
-      canceled: false,
-      completed: false,
-      createtime: '2025-01-01T00:00:04',
-    },
-    {
-      id: 6,
-      text: 'Learn Nuxt',
-      canceled: false,
-      completed: true,
-      createtime: '2025-01-01T00:00:05',
-    },
-  ]);
+  const [todos, setTodos] = useState([]);
 
   return (
     <div className='layout bg-[#555] h-screen xl:p5'>
